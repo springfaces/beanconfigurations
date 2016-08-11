30ce85c8ec369e50bef9ec978cd38652344b84b9
raghu <raghu.bogimi@gmail.com>
2016-08-11 16:07:56 -0500

    Added ApplicationContext featuren to App class and executed.

--- src/main/java/com/caveofprogramming/spring/App.java
+++ src/main/java/com/caveofprogramming/spring/App.java
@@ -1,6 +1,7 @@
 package com.caveofprogramming.spring;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
@@ -8,12 +9,12 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
  *
  */
 public class App {
-    public static void main( String[] args ) {
-      // ApplicationContext context  = new ClassPathXmlApplicationContext("beans.xml");
-    	 ApplicationContext context  = new FileSystemXmlApplicationContext("beans.xml");
-    	 Person person = (Person)context.getBean("person");
-     //	 Person perosn = context.getBean("person", Person.class);
-    	 person.speak();
-       ((FileSystemXmlApplicationContext)context).close();
-    }
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/caveofprogramming/spring/test/beans/beans.xml");
+		Person person = (Person) context.getBean("person");
+		/* you can you below statement if you want to avoid casting the Bean class */
+		//Person perosn = context.getBean("person", Person.class);
+		person.speak();
+	}
 }
