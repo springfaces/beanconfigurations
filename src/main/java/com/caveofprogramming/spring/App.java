54de6dd174fc6906351b86bf06db1c846bc40045
raghu <raghu.bogimi@gmail.com>
2016-08-11 16:26:18 -0500

    Constructor injection examples are included.

--- src/main/java/com/caveofprogramming/spring/App.java
+++ src/main/java/com/caveofprogramming/spring/App.java
@@ -13,8 +13,7 @@ public class App {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"com/caveofprogramming/spring/test/beans/beans.xml");
 		Person person = (Person) context.getBean("person");
-		/* you can you below statement if you want to avoid casting the Bean class */
-		//Person perosn = context.getBean("person", Person.class);
 		person.speak();
+		System.out.println(person);
 	}
 }
--- src/main/java/com/caveofprogramming/spring/Person.java
+++ src/main/java/com/caveofprogramming/spring/Person.java
@@ -1,8 +1,36 @@
 package com.caveofprogramming.spring;
 
 public class Person {
+
+	private int id;
+	private String name;
 	
+	public Person(int id, String name) {
+		this.id = id;
+		this.name= name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public void speak() {
 		System.out.println("Person speaking...!");
+	}
+	
+	public String toString() {
+		return "id : "+ id +", name: " +name;
 	}
 }
